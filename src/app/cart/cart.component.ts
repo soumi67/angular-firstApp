c3e87fc199cc879e5729ac1323d6ffd1653b368a
soumi67 <naqouri.salma1@gmail.com>
2022-02-21 00:14:09 +0100

    4 files modified, 6 added

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {}
+}
